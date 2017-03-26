6a17493b497591e9cc0da94f0b96f08dba1d272b
Jira Vinyoopongphan <jira.vinyoo@gmail.com>
2017-03-26 16:53:46 -0700

    Set up graphiql and graphql endpoints

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import path from 'path';
+import bodyParser from 'body-parser';
 import webpack from 'webpack';
 import webpackDev from 'webpack-dev-middleware';
 import webpackConfig from '../webpack.config.babel';
+import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
+import { schema } from './schema';
 
 const app = express();
 const compiler = webpack(webpackConfig);
@@ -13,6 +16,19 @@ app.use(webpackDev(compiler, {
 }));
 
 app.use(express.static(path.resolve(__dirname + '/../public')))
+
+/**
+ Set up GraphiQL and GraphQL
+ **/
+
+app.use('/graphiql', graphiqlExpress({
+  endpointURL: '/graphql'
+}));
+
+app.use('/graphql', bodyParser.json(), graphqlExpress({
+    schema,
+    context: {}
+}));
 
 app.get('/', (req, res, next) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
