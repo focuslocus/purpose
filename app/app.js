258b23dccd3220f6c2d5dca943a3faf720a60296
Jira Vinyoopongphan <jira.vinyoo@gmail.com>
2017-03-26 15:01:52 -0700

    Require React

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
-import { Component } from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
         <h1>Hello World</h1>
